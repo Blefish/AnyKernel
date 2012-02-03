3e345a88398aeaecbb80cce0818cca11d5092306
Rudolf Tammekivi <rtammekivi@gmail.com>
2012-02-03 12:07:03 +0200

    AnyKernel: Modify for U8800; removed unneeded binaries and modules; added older mkbootimg and unpackbootimg binaries; added newer update-binary;

--- kernel/mkbootimg.sh
+++ kernel/mkbootimg.sh
@@ -1,6 +1,10 @@
 #!/sbin/sh
 echo \#!/sbin/sh > /tmp/createnewboot.sh
-echo /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base $(cat /tmp/boot.img-base) --output /tmp/newboot.img >> /tmp/createnewboot.sh
+mkdir /boot
+mount /dev/block/mmcblk0p1 /boot
+/tmp/unpackbootimg -i /boot/image/boot.img -o /tmp/
+echo /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base $(cat /tmp/boot.img-base) --pagesize $(printf "%d" 0x`cat /tmp/boot.img-pagesize`) --output /tmp/newboot.img >> /tmp/createnewboot.sh
 chmod 777 /tmp/createnewboot.sh
 /tmp/createnewboot.sh
-return $?
+cp /tmp/newboot.img /boot/image/boot.img
+umount /boot
